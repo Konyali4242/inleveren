a1238966cda86e17767baa350d24febda73ee639
Konyali4242 <132984317+Konyali4242@users.noreply.github.com>
2023-05-09 08:51:23 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,37 @@
+<?php 
+    // $waarde1 = 12;
+    // $waarde2 = 10;
+
+    // $resultaat = $waarde1 + $waarde2;
+
+    // echo "$waarde1 + $waarde2 = $resultaat";
+
+
+
+    // $waarde3 = 12;
+    // $waarde4 = 10;
+
+    // $resultaat2 = $waarde3 * $waarde4;
+
+    // echo "$waarde3 * $waarde4 = $resultaat2";
+
+
+
+    // $name = "Uveys";
+
+    // echo "Welkom " . $name;
+
+
+
+    //<?php
+
+    // deze code bevat veel fouten
+    //haal alle fouten eruit!
+    //
+
+    // $mijnNaam="Carl";
+    // $jouwNaam="Camel";
+    // echo $mijnNaam . " zit bij " . $jouwNaam . " in de klas!";
+
+    // ? >
+?>
