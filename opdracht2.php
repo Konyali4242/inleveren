a537da08410de530ea426e2dd96781ae7f01cd64
Konyali4242 <132984317+Konyali4242@users.noreply.github.com>
2023-05-11 22:59:11 +0200

    Add files via upload

---
+++ opdracht2.php
@@ -0,0 +1,52 @@
+<?php 
+
+/*
+
+$variabele1 = 10;
+$variabele2 = 10;
+
+if($variabele1 == $variabele2) {
+    echo "De twee waarden zijn gelijk";
+}
+
+*/
+
+
+
+/*
+
+$variabele1 = 10;
+$variabele2 = 10;
+
+if($variabele1 == 12) {
+    echo "De twee waarden zijn ongelijk";
+}
+
+*/
+
+
+
+/*
+$variabele1 = 10;
+$variabele2 = 10;
+
+if($variabele1 == $variabele2) {
+    echo "De twee waarden zijn gelijk";
+} else {
+    echo "De twee waarden zijn ongelijk";
+}
+*/
+
+
+/*
+$variabele1 = 10;
+$variabele2 = 10;
+
+if($variabele1 == 9) {
+    echo "De twee waarden zijn gelijk";
+} else {
+    echo "De twee waarden zijn ongelijk";
+}
+*/
+
+?>
