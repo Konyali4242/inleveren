49b54ea7cb3790d97b0c9a3bcf3b41935d31dab5
Konyali4242 <132984317+Konyali4242@users.noreply.github.com>
2023-05-12 15:48:31 +0200

    Add files via upload

---
+++ post.php
@@ -0,0 +1,40 @@
+<?php
+$naam = $_POST["naam"];
+$achternaam = $_POST["achtermaa,"];
+$leeftijd = $_POST["leeftijd"];
+$adres = $_POST["adres"];
+$email = $_POST["emial"];
+
+
+echo $naam . " " . $achternaam . " " . $leeftijd . " " . $adres . " " . $email;
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+
+<form method="POST">
+
+<input type="text" name="naam" placeholder="Firstname">
+<input type="text" name="achternaam" placeholder="Lastname">
+<input type="text" name="leeftijd" placeholder="Age">
+<input type="text" name="adres" placeholder="Adress">
+<input type="email" name="email" placeholder="Mail">
+
+<form>
+
+</body>
+
+</html>
