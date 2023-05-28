14efc5aa44636de748b631d2fe0b4ee6d1f5f7e5
Konyali4242 <132984317+Konyali4242@users.noreply.github.com>
2023-05-28 23:49:41 +0200

    ArraysWeek3PHP

---
+++ PHPweek3.php
@@ -0,0 +1,55 @@
+<?php
+
+// DEEL1
+
+// $myArray = ['auto','fiets','brommer','bus','vliegtuig','trein'];
+// $myArray[] = 'scooter';
+
+// echo $MyArray[0];
+// echo $MyArray[1];
+// echo $MyArray[2];
+// echo $MyArray[3];
+// echo $MyArray[4];
+// echo $MyArray[5];
+// echo $MyArray[6];
+
+// DEEL2
+
+// $myArray = ['auto','fiets','brommer','bus','vliegtuig','trein'];
+
+// $count = count($myArray);
+// echo "er zijn $count elementen in het Array";
+
+// $myArray[] = 'scooter';
+
+// echo "er zijn $count2 elementen in het Array";
+
+// DEEL3
+
+//2 =   "bar" element wordt uitgeprint
+//5 =   4 wordt uitgeprint
+//8 =   Toyota wordt uitgeprint
+//9 =   3 arrays worden opgeteld
+//12 =  5 wordt uitgeprint
+//19 =  5 wordt uitgeprint
+
+// DEEL4 A
+
+// $cijfersPHP = [4.4, 4.6, 5.6, 6.1, 7.6, 7.2];
+
+// $gemiddeld = array_sum($cijfersPHP) / count($cijfersPHP);
+
+// echo "Gemiddelde is; ".round($gemiddeld, 1);
+
+
+/*
+DEEL4 B
+
+opdracht B: ik vind het beter als het samengevoegd zijn want 
+dan ziet de code meer netjes eruit
+
+maar als het apart is is het meer begrijpelijk 
+voor degenen die nieuw zijn in PHP coderen
+
+*/
+?>
