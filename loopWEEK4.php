65fd69de3f0e5d241d4ed75526647db98dc1f816
Konyali4242 <132984317+Konyali4242@users.noreply.github.com>
2023-05-30 20:41:03 +0200

    Add files via upload

---
+++ loopWEEK4.php
@@ -0,0 +1,31 @@
+<?php
+
+// opdracht1
+// for ($x = 0; $x <= 50; $x++) {
+// echo $x . " ";
+// }
+
+
+// opdracht2
+// $klasgenoten = ["Zee", "Lakshya", "Daniel", "Ghor", "Mohamed", "Marouane", "Mehmet", "Burak", "Lionel", "Gorav"];
+
+// foreach ($klasgenoten as $naam) {
+    // echo $naam . "<br>";
+// }
+
+// opdracht3
+// $maanden = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December']; 
+
+// for ($q = 0; $q < count($maanden); $q++) {
+    // echo "Maand ".($q+1)." is ".$maanden[$q].".<br>";
+// }
+
+// opdracht4
+
+// $maanden = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December']; 
+// $i = 1;
+// foreach ($maanden as $maand) {
+    // echo "Maand ".($i++)." is ".$maand.".<br>";
+// }
+
+?>
