c39f814a64cd0960d64558cd6823971304a68e03
Konyali4242 <132984317+Konyali4242@users.noreply.github.com>
2023-06-16 13:40:15 +0200

    Add files via upload

---
+++ FunctionWEEK6.php
@@ -0,0 +1,58 @@
+<?php
+
+// OPDRACHT1
+
+// $tijd = date("g:i a");
+
+// if ($tijd < 12) {
+//     echo "goedemorgen";
+// } 
+// elseif ($tijd < 18) {
+    // echo "goedemiddag";
+// } 
+// else {
+    // echo "goedenavond";
+// }
+
+
+
+// OPDRACHT2
+
+// function opdracht2($nummer1, $nummer2) {
+//     $resultaat = $nummer1 * $nummer2;
+//     return $resultaat;
+// }
+
+// echo "De resultaat is " . opdracht2(6,7);
+
+
+
+// OPDRACHT3
+
+// $datumvannu = date("Y-m-d");
+// $einddatum = date('Y-m-d', strtotime('12/31'));
+// $verschil = date_diff(date_create($datumvannu), date_create($einddatum));
+
+// echo $verschil->format('%a');
+
+
+
+// OPDRACHT4
+
+// function berekenlengte($strings) {
+//     $totaallengte = 0;
+
+//     foreach ($strings as $string) {
+//         $lengte = strlen($string);
+//         $totaallengte += $lengte;
+//     }
+
+//     return $totaallengte;
+// }
+
+// $array = ["Hoe gaat het", "Uveys", "Hallo Wereld"];
+// $totaallengte = berekenlengte($array);
+// echo "De totale lengte van alle strings is: $totaallengte";
+
+
+?>
