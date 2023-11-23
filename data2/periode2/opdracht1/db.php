d6ee0d9d2173df874c36708ee1697b325c51f73d
Konyali4242 <132984317+Konyali4242@users.noreply.github.com>
2023-11-23 12:17:28 +0100

    Update db.php

--- data2/periode2/opdracht1/db.php
+++ data2/periode2/opdracht1/db.php
@@ -1,24 +1,17 @@
 <?php
 
-$host = 'localhost:3307';
-$db = 'winkel';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
+class database {
+    public $pdo;
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
-];
-
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-    echo "Connected to database winkel.";
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    public function __construct ($db="test", $host="localhost",  $user="root", $pass="") {
+        try{
+            $this->pdo = new PDO ("mysql:$host=;dbname=$db", $user, $pass);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected";
+        } catch (Exception $e) {
+            echo "Error" . $e->getMessage();
+        }
+    }
 }
 
 ?>
