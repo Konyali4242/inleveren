abfe24a8f3b8a9b6c44b9a1f02abfed96a309efa
Konyali4242 <132984317+Konyali4242@users.noreply.github.com>
2023-11-24 11:59:00 +0100

    Update db.php

--- data2/periode2/opdracht1/db.php
+++ data2/periode2/opdracht1/db.php
@@ -3,7 +3,7 @@
 class database {
     public $pdo;
 
-    public function __construct ($db="test", $host="localhost",  $user="root", $pass="") {
+    public function __construct ($db="test", $host="localhost:3307",  $user="root", $pass="") {
         try{
             $this->pdo = new PDO ("mysql:$host=;dbname=$db", $user, $pass);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
